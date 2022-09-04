5243b8740f65d511f1a42dbbf99618bd916167fe
Darren <you@example.comdarren@kandekore.net>
2022-09-04 16:49:57 +0100

    basic functionality working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# -Professional-README-Generator
+success
+    <h1>peter pan</h1>
+    <p>pete@pan</p>
+    <p>jjsvv</p>
+
--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+const generateReadMe = ({ username, github, linkedin }) => `success
+    <h1>${username}</h1>
+    <p>${github}</p>
+    <p>${linkedin}</p>
+
+`;
+
+inquirer
+  .prompt([
+    {
+      name: "username",
+      message: "what is your name?",
+      type: "input",
+    },
+    {
+      name: "github",
+      message: "what is your Git Hub URL??",
+      type: "input",
+    },
+    {
+      name: "linkedin",
+      message: "what is your Linkedin URL??",
+      type: "input",
+    },
+  ])
+  .then((response) => {
+    const text = generateReadMe(response);
+    fs.writeFile("README.md", text, (err) => {
+      err ? console.log(err) : console.log("success");
+    });
+  });
