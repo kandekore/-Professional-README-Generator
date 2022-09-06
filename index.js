f3d60f31c0dbeeb7bd7dc7f6fcc5a17dda679ca5
Darren <you@example.comdarren@kandekore.net>
2022-09-06 12:52:59 +0100

    For loop attempt

--- index.js
+++ index.js
@@ -1,34 +1,99 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const generateReadMe = ({ username, github, linkedin }) => `success
-    <h1>${username}</h1>
-    <p>${github}</p>
-    <p>${linkedin}</p>
+const generateReadMe = ({
+  title,
+  description,
+  motivation,
+  why,
+  problem,
+  learned,
+}) => `
+# ${title}
 
+## Description
+
+${description}
+
+- The Motivation for building this application was  ${motivation}
+- The application was built to ${why}
+- It solves the problem of ${problem}
+- Through the process of building this application I learned ${learned}
 `;
 
-inquirer
-  .prompt([
-    {
-      name: "username",
-      message: "what is your name?",
-      type: "input",
-    },
-    {
-      name: "github",
-      message: "what is your Git Hub URL??",
-      type: "input",
-    },
-    {
-      name: "linkedin",
-      message: "what is your Linkedin URL??",
-      type: "input",
-    },
-  ])
-  .then((response) => {
-    const text = generateReadMe(response);
-    fs.writeFile("README.md", text, (err) => {
-      err ? console.log(err) : console.log("success");
-    });
+inquirer.prompt([
+  {
+    name: "title",
+    message: "What is the project title?",
+    type: "input",
+  },
+  {
+    name: "description",
+    message: "Give the project a description",
+    type: "input",
+  },
+  {
+    name: "motivation",
+    message: "The motivation for building this app was...",
+    type: "input",
+  },
+  {
+    name: "why",
+    message: "The application was built to...",
+    type: "input",
+  },
+  {
+    name: "problem",
+    message: "The application solves the problem of...",
+    type: "input",
+  },
+  {
+    name: "installation",
+    message: "How is this application installed?",
+    type: "input",
+  },
+
+  {
+    name: "usage",
+    message: "How is the application used?",
+    type: "input",
+  },
+
+  {
+    name: "screenshot",
+    message: "Provide the URL to a screenshot",
+    type: "input",
+  },
+  {
+    name: "license",
+
+    message:
+      "Choose a license for the application. (navigate using up & down arrow)",
+    type: "list",
+    choices: ["Apache License 2.0", "GNU GPLv3", "MIT", "ISC License"],
+  },
+  {
+    name: "install",
+    message: "How many steps required to install your application?",
+    type: "number",
+  },
+]);
+// function contributors () {
+//   var install = `${install}`
+//   for(let i=0; i<install.length; i++) {
+//     inquirer
+// .prompt([
+
+//     name: "install"[i],
+//     message: "How many steps required to install your application?",
+//     type: "number",
+
+//   },
+// ]);
+
+contributors().then((response) => {
+  const text = generateReadMe(response);
+  fs.writeFile("README.md", text, (err) => {
+    err ? console.log(err) : console.log("success");
   });
+});
