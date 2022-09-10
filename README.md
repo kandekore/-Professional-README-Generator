69f21b8daedbdcb0328594e564b313be3266021e
Darren Kandekore <darren@kandekore.net>
2022-09-10 17:02:10 +0100

    licence if

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 
-# p2  
+##README CREATE
+# undefined  
 undefined
 ## Description
-Apache License 2.0
+undefined
 2
 undefined
 undefined
@@ -10,19 +11,37 @@ undefined
 
 
 
-
-- The Motivation for building this application was  
-- The application was built to 
-- It solves the problem of 
+undefined
+- The Motivation for building this application was  undefined
+- The application was built to undefined
+- It solves the problem of undefined
 - Through the process of building this application I learned undefined
 
+##APPEND1
 
-p2
+undefined
 ## Description
 
 
 
-s1
+#APPEND2
+
 undefined
+undefined
+undefined
+## Step
+
+
+
+#APPEND 3
+undefined
+
+## Step
+
+
+
+#APPEND 4
+http://image.com
+
 ## Step
 
--- new.js
+++ new.js
@@ -15,6 +15,7 @@ const generateReadMe = ({
   step1,
   contributors,
 }) => `
+##README CREATE
 # ${title}  
 ${title1}
 ## Description
@@ -47,71 +48,93 @@ const appendReadMe = ({
   step1,
   contributors,
 }) => `
+##APPEND1
 
 ${title1}
 ## Description
 
 `;
-const appendReadMe2 = ({ step1, step2 }) => `
+const appendReadMe2 = ({ step1, step2, arr }) => `
 
+#APPEND2
+
+${arr}
 ${step1}
 ${step2}
 ## Step
 
 `;
+const appendReadMe3 = ({ contname }) => `
+
+#APPEND 3
+${contname}
+
+## Step
+
+`;
+
+const appendReadMe4 = ({ license }) => `
+
+#APPEND 4
+${license}
+
+## Step
+
+`;
+
 
 inquirer
   .prompt([
-    {
-      name: "title",
-      message: "What is the project title?",
-      type: "input",
-    },
-    {
-      name: "description",
-      message: "Give the project a description",
-      type: "input",
-    },
-    {
-      name: "motivation",
-      message: "The motivation for building this app was...",
-      type: "input",
-    },
-    {
-      name: "why",
-      message: "The application was built to...",
-      type: "input",
-    },
-    {
-      name: "problem",
-      message: "The application solves the problem of...",
-      type: "input",
-    },
-    {
-      name: "installation",
-      message: "How is this application installed?",
-      type: "input",
-    },
+    // {
+    //   name: "title",
+    //   message: "What is the project title?",
+    //   type: "input",
+    // },
+    // {
+    //   name: "description",
+    //   message: "Give the project a description",
+    //   type: "input",
+    // },
+    // {
+    //   name: "motivation",
+    //   message: "The motivation for building this app was...",
+    //   type: "input",
+    // },
+    // {
+    //   name: "why",
+    //   message: "The application was built to...",
+    //   type: "input",
+    // },
+    // {
+    //   name: "problem",
+    //   message: "The application solves the problem of...",
+    //   type: "input",
+    // },
+    // {
+    //   name: "installation",
+    //   message: "How is this application installed?",
+    //   type: "input",
+    // },
 
-    {
-      name: "usage",
-      message: "How is the application used?",
-      type: "input",
-    },
+    // {
+    //   name: "usage",
+    //   message: "How is the application used?",
+    //   type: "input",
+    // },
 
-    {
-      name: "screenshot",
-      message: "Provide the URL to a screenshot",
-      type: "input",
-    },
-    {
-      name: "license",
+    // {
+    //   name: "screenshot",
+    //   message: "Provide the URL to a screenshot",
+    //   type: "input",
+    // },
+    // {
+    //   name: "license",
 
-      message:
-        "Choose a license for the application. (navigate using up & down arrow)",
-      type: "list",
-      choices: ["Apache License 2.0", "GNU GPLv3", "MIT", "ISC License"],
-    },
+    //   message:
+    //     "Choose a license for the application. (navigate using up & down arrow)",
+    //   type: "list",
+    //   choices: ["Apache License 2.0", "GNU GPLv3", "MIT", "ISC License"],
+    // },
     {
       name: "install",
       message: "How many steps required to install your application?",
@@ -140,11 +163,6 @@ inquirer
     inquirer
       .prompt([
         {
-          name: "title1",
-          message: "What is the project title?",
-          type: "input",
-        },
-        {
           name: "contributors",
           message: "How many contributors?",
           type: "number",
@@ -157,20 +175,62 @@ inquirer
           var responseTwo = await inquirer.prompt([
             {
               type: "input",
-              name: "cont name" + (x + 1),
-              message: "Answer the question" + (x + 1),
+              name: "contname" ,
+              message: "contributor " + (x + 1) + " name",
             },
             {
               type: "input",
-              name: "cont github" + (x + 1),
+              name: "contgithub",
               message: "Answer the question" + (x + 1),
-            },
+            },        
           ]);
-          var obj2 = responseTwo;
+
+          console.log(responseTwo)       
+        console.log(responseTwo.contname)
+        console.log(responseTwo.contgithub)
+
+        console.log("contname"+[x]+":"+responseTwo.contname)
+          
+          var obj2 = (responseTwo)
 
           arr2.push(obj2);
-          console.log(responseTwo);
+          console.log(responseTwo.name)
+          console.log(responseTwo.value)
+          var results2 = answers2;
         }
+
+       
+        // .then(async (answers2) => {
+          var lic = [];
+          var licResponse = await inquirer.prompt ([
+            {
+              name: "license",
+        
+              message:
+                "Choose a license for the application. (navigate using up & down arrow)",
+              type: "list",
+              choices: [{name : "Apache License 2.0", value: "http://image.com"},  {name : "GNU GPLv3",  value: "http://image1.com"}],
+            },
+          ])
+          //  if (licResponse == ({ license: 'Apache License 2.0' })){
+          //  var licImg = "http://image.com"
+          //  }
+          //  else if (licResponse == ({ license: 'GNU GPLv3' })){
+          //  var licImg = "http://image1.com"
+          //  }
+
+           console.log(licResponse)
+          
+          //  lic.push(licImg)
+      //      console.log(licImg)
+           
+      // // })
+       
+      // console.log(licImg)
+           
+      console.log(lic)
+        console.log("results2");
+        console.log(results2);
         console.log(response);
         var results = answers;
         // response.push(answers);
@@ -191,7 +251,9 @@ inquirer
         var results = answers;
         const text = generateReadMe(results);
         const text2 = appendReadMe(answers2);
-        const text3 = appendReadMe2(...arr);
+        const text3 = appendReadMe2(arr);
+        const text4 = appendReadMe3(arr2);
+        const licImg = appendReadMe4(licResponse)
 
         // console.log((results, answers2).replace(/} { /g, ""));
         // const moreText = generateReadMe(answers2);
@@ -203,6 +265,12 @@ inquirer
         });
 
         fs.appendFile("README.md", text3, (err) => {
+          err ? console.log(err) : console.log("success");
+        });
+        fs.appendFile("README.md", text4, (err) => {
+          err ? console.log(err) : console.log("success");
+        });
+        fs.appendFile("README.md", licImg, (err) => {
           err ? console.log(err) : console.log("success");
         });
       });
